21fe2bedd66cbe78bb4cd4e6af1ab1a7e4de1c4e
tuanl12 <38969089+tuanl12@users.noreply.github.com>
2018-05-24 01:07:14 -0400

    Add files via upload

--- sampling.R
+++ sampling.R
@@ -13,19 +13,20 @@ library(coda)
 rstan_options(auto_write = TRUE)
 options(mc.cores = parallel::detectCores())
 
-#vector to store the MAPE
+#vector to store the MAPE and MSE error for each of 365 days. 
 made_err <- rep(0,365)
 mse_err <- rep(0,365)
-#data preparation for inputting into Stan
+#data preparation for inputting into Stan. Running for loops over 365 days
 for(i in 1:365) {
+  #denote the dimensions for true demand vector yT (denoted as d' in the paper) and ON counts data x
   nrow = nrow(rte_m[[i]]);
   ncol = ncol(rte_m[[i]]);
   #assign routing matrix to a new variable A (same notation as in the paper)
   A <- as.matrix(rte_m[[i]]);
-  #variance vectors sigma_x, sigma_y, both drawn from U(0,10) for prior and likelihood.
+  #variance vectors sigma_x^2, sigma_y^2 (both drawn from U(0,10)) for prior and likelihood (denoted the same as in the paper).
   #sigma_x <- as.vector(sample.int(10, nrow(kf_vect[[i]]), replace=TRUE))
   sigma_y <- as.vector(sample.int(10, nrow(kf_vect[[i]]), replace=TRUE))
-  #generate historical demand denoted as "dh" in the paper (labeled as yH in the code)
+  #assign historical demand, which is the solution given by least-squares, into variable "yh" (denoted as "dh" in the paper) 
   yH <- as.vector(dh_vect[[i]]$X);
   #create a simulated true demand "yT" (denoted as d' in the paper) with formula "yH + epsilon" where epsilon ~ U(0,10)
   yT <- yH + as.vector(eps_vect[[i]]); 
@@ -33,12 +34,14 @@ for(i in 1:365) {
   epsilon <- sample.int(20, nrow(kf_vect[[i]]), replace=TRUE)
   #noise (denoted as 'k in the paper) added to the variance of the normal distribution approximation for Poisson.
   #epsilon2 <- runif(nrow(kf_vect[[i]]),0.02, 0.9)
-  #simulate the currently observed counts data "x" (denoted as d' in the paper) by the formula: A*yT + epsilon 
+  #simulate the currently observed ON counts data "x" (denoted as d' in the paper) by the formula: A*yT + epsilon 
   #(epsilon accounts for noises in our currently observed counts data 'x')
   x <- round(as.vector(as.matrix(rte_m[[i]])%*%yT) + epsilon)
+  #denote number of iterations for each time one MCMC chain is run
   iterations = 250;
   
   #input our Stan model file "stamodeling.stan" (for the two cases when the likelihood ~ Normal(A*d+epsilon_1, sigma_1), as denoted in the paper).
+  #input our Stan model file "poissnorm.stan" (for the two cases when the likelihood ~ Poisson(A*d+epsilon_1))
   stanmodel1 <- stan_model(file = "poissnorm.stan",
                            model_name = "stanmodel1");
   
@@ -55,13 +58,15 @@ for(i in 1:365) {
   #store the graph on the .jpeg file titled "Comparison_KFnnls_Day..."
   file_name10 <- paste("Comparison_KFnnls_Day", i, ".jpeg")
   jpeg(file_name10)
-  #create comparison plots between estimated demand(colMeans(posterior_yTtheta[,3,])), Kalman-Filter (only for Normal - Normal case), 
+  #create comparison plots between estimated demand (colMeans(posterior_yTtheta[,3,])), Kalman-Filter (only for Normal - Normal case), 
   #and simulated true demand yT 
   plot(colMeans(posterior_yTtheta[,3,]),col=1, type = 'l', ylab="d", xlab = "Zone index")
   lines(yT,col=2, type = 'l', ylab = "d")
+  #only use when running for the Normal - Normal case. 
   #lines(mean_kf[[i]], col=3, type = 'l', ylab = "d")
   #choose the first one for the 3 cases (Normal - Poisson/Poisson - Poisson/Poisson - Normal) and plot the estimated demand d versus true demand d'
   legend(x = "topleft",legend = c("recovered", "true"), col=1:2, lty=c(1,1))
+  #this one is only for the first case (Normal - Normal) since there is a Kalman-Filter solution in that scenario.
   #legend(x = "topleft",legend = c("recovered", "true", "Kalman-Filter"), col=1:3, lty=c(1,1,1))
   
   #compute MAPE using the formula: 1/N*sum(absolute((y_predicted - y_true)/y_true))
@@ -89,4 +94,3 @@ for(i in 1:365) {
   print(rhat_plt)
   dev.off()
 }
-
